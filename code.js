5482fcd2eaedd093596c589e8c3b99f5cc94c5dc
acraig9266 <158104649+acraig9266@users.noreply.github.com>
2024-08-28 22:23:22 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,10 +1,10 @@
 function binarySearch(list, element) {
     let first = 0;
     let last = (list.length - 1);
-    let center = (list.length / 2);
+    let center;
 
-    while (first < last) {
-        center = (first + last) / 2;
+    while (first <= last) {
+        center = Math.floor((first + last) / 2);
         if (element == list[center]) { return center }
         else if (element >= list[center]) { first = center + 1}
         else if (element <= list[center]) { last = center - 1}
